a84faf1b2a78bc71de86c9a4af2e26f7c071e086
Albertik1 <emty0329@gmail.com>
2021-01-29 15:28:55 +0800

    Albert

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import SearchForm from './Components/SearchForm';
+import RepoList from './Components/RepoList';
+
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      repos: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    this.performSearch();
+
+  }
+
+
+  performSearch = (query = 'web') => {
+    axios.get(`https://api.github.com/search/repositories?q=${query}`)
+        .then(response => {
+          this.setState({
+            query: query,
+            repos: response.data.items,
+            loading: false
+          });
+        })
+        .catch(error => {
+          console.log('Error fetching and parsing data', error);
+        });
+  }
+
+  render() {
+    return (
+        <div>
+          <div className="main-header">
+            <div className="inner">
+              <h1 className="main-title">RepoSearch</h1>
+              <SearchForm onSearch={this.performSearch} />
+              <section className='section'>
+
+              </section>
+            </div>
+          </div>
+          <div className="main-content">
+            {
+              (this.state.loading)
+                  ? <p>Loading...</p>
+                  : <div><h2>{this.state.query}</h2><RepoList data={this.state.repos} /></div>
+            }
+          </div>
+
+        </div>
+
+    );
+  }
+}
